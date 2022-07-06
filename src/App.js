c9dc56f843c4bc84fffc57d8272da03218a634bb
sydney257 <sydmercad@gmail.com>
2022-07-06 13:38:07 -0700

    Added closing link to GitHub repo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import "./App.css";
 import Weather from "./Weather";
 
 function App() {
-  return <Weather />;
+  return (
+    <div>
+      <Weather />
+      <p className="credentials">
+        <a
+          href="https://github.com/sydney257/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced code
+        </a>{" "}
+        by Sydney Mercado from <a href="https://shecodes.io">SheCodes</a>
+      </p>
+    </div>
+  );
 }
 
 export default App;
